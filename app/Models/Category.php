d6002670ef6c6296d9f09f79b6191df14913d732
Hardik Kanzariya <hardik.kanzariya@yudiz.com>
2025-03-17 17:54:45 +0530

    category display and create menu left side

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Enum\StatusEnums;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ["name", "status"];
+
+    // public function products()
+    // {
+    //     return $this->hasMany(Product::class);
+    // }
+
+    //create a scope for only status is active 
+    public function scopeActive($query)
+    {
+        return $query->where('status', StatusEnums::ACTIVE->value);
+    }
+}
